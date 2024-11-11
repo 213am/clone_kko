1442cf17e41e04d5afba180d9f8876fc122c8dfa
213am <dev.213am@gmail.com>
2024-11-11 15:53:36 +0900

    [javaScript] api 연동 작업중

--- js/slide-main.js
+++ js/slide-main.js
@@ -1,25 +1,54 @@
 window.addEventListener("load", function () {
-  const visualSlideSwiper = new Swiper(".visual-slide", {
-    loop: true,
-    pagination: {
-      el: ".visual-slide .swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    speed: 400,
-  });
-  // 개발자 추가 작업 : 마우스가 enter(마우스를 올리고 있으면)가 되면
-  const visualSlide = document.querySelector(".visual-slide");
-  // console.log(visualSlide);
-  visualSlide.addEventListener("mouseenter", function () {
-    // console.log("mouseenter");
-    visualSlideSwiper.autoplay.stop();
-  });
-  visualSlide.addEventListener("mouseleave", function () {
-    // console.log("mouseleave");
-    visualSlideSwiper.autoplay.start();
-  });
+  const SLIDE_DATA_URL = "/apis/mainSlide.json";
+
+  fetch(SLIDE_DATA_URL)
+    .then(function (response) {
+      // console.log(response);
+      const result = response.json();
+      return result;
+    })
+    .then(function (result) {
+      console.log(result);
+      let htmlMainSlide = "";
+
+      for (i = 0; i < result.length; i++) {
+        const obj = result[i];
+
+        const tag = `
+          <div class="swiper-slide">
+            <a href="${obj.url}">
+              <img src="./images/${obj.pic}" alt="${obj.title}" />
+            </a>
+          </div>
+        `;
+        htmlMainSlide += tag;
+      }
+      const htmlMainSlideTag = document.querySelector("#visual-slide-api");
+      htmlMainSlideTag.innerHTML = htmlMainSlide;
+
+      const visualSlideSwiper = new Swiper(".visual-slide", {
+        loop: true,
+        pagination: {
+          el: ".visual-slide .swiper-pagination",
+          clickable: true,
+        },
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        speed: 400,
+      });
+      // 개발자 추가 작업 : 마우스가 enter(마우스를 올리고 있으면)가 되면
+      const visualSlide = document.querySelector(".visual-slide");
+      // console.log(visualSlide);
+      visualSlide.addEventListener("mouseenter", function () {
+        // console.log("mouseenter");
+        visualSlideSwiper.autoplay.stop();
+      });
+      visualSlide.addEventListener("mouseleave", function () {
+        // console.log("mouseleave");
+        visualSlideSwiper.autoplay.start();
+      });
+    })
+    .catch(function () {});
 });
